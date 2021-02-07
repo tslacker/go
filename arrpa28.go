8c99eb4b067495bd6961b0db8571f4533eb3f609
tslacker <longinus0@hotmail.com>
2021-02-07 13:43:34 +0900

    test

---
+++ arrpa28.go
@@ -0,0 +1,26 @@
+package arrpa28
+
+import "fmt"
+
+func main() {
+	var s, p, ans int
+	fmt.Scan(&s, &p)
+	a := make(map[int]int)
+	a[0] = s
+	for i := 0; i <= p; i++ {
+		for j := 0; j+i <= p; j++ {
+			a[i+j] = Max(a[i+j], a[i]*(100+j)/100)
+		}
+	}
+	for _, i := range a {
+		ans = Max(ans, i)
+	}
+	fmt.Println(ans)
+}
+
+func Max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
