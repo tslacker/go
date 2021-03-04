27970d1221b2954e0734babab8e38f92f8e53381
tslacker <longinus0@hotmail.com>
2021-03-04 16:28:08 +0900

    test

--- echo/main.go
+++ echo/main.go
@@ -4,14 +4,24 @@ import (
 	"fmt"
 	"time"
 
+	"net/http"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
+
+	"github.com/labstack/echo/v4"
 )
 
 type User struct {
 	Id       int64  `xorm:"'id'"`
 	Name     string `xorm:"'name'"`
 	Password string `xorm:"'password'"`
+}
+
+type Person struct {
+	Id       int64  `json:"id"`
+	Name     string `json:"name"`
+	Password string `json:"password`
 }
 
 func main() {
@@ -21,6 +31,7 @@ func main() {
 
 	_ = engine.Sync2(new(User))
 
+	e := echo.New()
 	// for j := 0; j < 10; j++ {
 	// 	var hoge User
 	// 	var user []User
@@ -31,9 +42,28 @@ func main() {
 	// 	}
 	// 	engine.Insert(&user)
 	// }
-	var user []User
-	_ = engine.Table("user").Find(&user)
-	fmt.Println(user)
+	// var user []User
+	// _ = engine.Table("user").Find(&user)
+	// fmt.Println(user)
 	end := time.Now()
 	fmt.Printf("%f秒\n", (end.Sub(start)).Seconds())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"hello": "world"})
+	})
+	e.GET("/aaaa", func(c echo.Context) error {
+		var user []User
+		var hoge User
+		var b Person
+		_ = engine.Table("user").Find(&user)
+		hoge.Name = "name"
+		hoge.Password = "huga"
+		b.Name = hoge.Name
+		b.Password = hoge.Password
+		b.Name = "name"
+		b.Password = "huga"
+		fmt.Println(user)
+		return c.JSON(http.StatusOK, user)
+	})
+	e.Logger.Fatal(e.Start(":1313"))
 }
