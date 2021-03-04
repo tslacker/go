40aee674f8a950ea8bcde1098e468f3916691088
tslacker <longinus0@hotmail.com>
2021-03-04 16:28:08 +0900

    test

--- echo/main.go
+++ echo/main.go
@@ -1,39 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
+
+	"github.com/labstack/echo/v4"
 )
 
 type User struct {
-	Id       int64  `xorm:"'id'"`
-	Name     string `xorm:"'name'"`
-	Password string `xorm:"'password'"`
+	Id       int64  `xorm:"pk autoincr not null" json:"id"`
+	GameName string `xorm:"not null" json:"gamename"`
+	Name     string `xorm:"not null" json:"name"`
+	Password string `xorm:"not null" json:"password`
 }
 
 func main() {
-	start := time.Now()
 	// engine, _ := xorm.NewEngine("mysql", "root:test@tcp([127.0.0.1]:3306)/test?charset=utf8mb4&parseTime=true")
 	engine, _ := xorm.NewEngine("mysql", "root:test@tcp(localhost:3306)/test?charset=utf8mb4&parseTime=true")
 
 	_ = engine.Sync2(new(User))
 
-	// for j := 0; j < 10; j++ {
-	// 	var hoge User
-	// 	var user []User
-	// 	for i := 0; i < 50000; i++ {
-	// 	hoge.Name = "name"
-	// 	hoge.Password = "huga"
-	// 	user = append(user, hoge)
-	// 	}
-	// 	engine.Insert(&user)
-	// }
-	var user []User
-	_ = engine.Table("user").Find(&user)
-	fmt.Println(user)
-	end := time.Now()
-	fmt.Printf("%f秒\n", (end.Sub(start)).Seconds())
+	e := echo.New()
+
+	e.POST("/insert", func(c echo.Context) error {
+		post := new(User)
+		c.Bind(post)
+		engine.Insert(post)
+		return c.JSON(http.StatusOK, post)
+	})
+
+	e.GET("/findall", func(c echo.Context) error {
+		var user []User
+		engine.Table("user").Find(&user)
+		return c.JSON(http.StatusOK, user)
+	})
+
+	e.GET("/find", func(c echo.Context) error {
+		var user []User
+		engine.Table("user").Where("gamename = ?", c).Find(&user)
+		return c.JSON(http.StatusOK, user)
+	})
+
+	e.Logger.Fatal(e.Start(":1313"))
 }
